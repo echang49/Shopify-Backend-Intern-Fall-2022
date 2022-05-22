2d58d2ef2b24e4723705534a41e63a523c85a746
Edward <46017382+echang49@users.noreply.github.com>
2022-05-22 19:22:18 -0400

    Add Testing Framework
    Adding Jest and configuring it to work with typescript and ESM.

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
   PORT: 5000,
-  ITEM_DATABASE: './items.json',
-  DELETED_ITEM_DATABASE: './deleted_items.json',
-  SHIPMENT_DATABASE: './shipments.json',
+  ITEM_DATABASE: 'items.json',
+  DELETED_ITEM_DATABASE: 'deleted_items.json',
+  SHIPMENT_DATABASE: 'shipments.json',
 };
 
 export default config;
