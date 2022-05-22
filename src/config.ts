ab73f180a4a05c683565b5ad32ad0eeee2a21346
Edward <46017382+echang49@users.noreply.github.com>
2022-05-22 02:53:17 -0400

    TS CommonJS to ESM

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
     PORT: 5000,
-    ITEM_DATABASE: "",
-    DELETED_ITEM_DATABASE: "",
-    SHIPMENT_DATABASE: ""
+    ITEM_DATABASE: "./items.json",
+    DELETED_ITEM_DATABASE: "./deleted_items.json",
+    SHIPMENT_DATABASE: "./shipments.json"
 };
 
 export default config;
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { Shipment } from './routes/Shipment';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/inventory', Inventory.router);
 app.use('/api/shipment', Shipment.router);
 
