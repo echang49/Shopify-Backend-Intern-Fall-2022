8b627720141d9351dfbc33430924825d9300c6cb
Edward <46017382+echang49@users.noreply.github.com>
2022-05-22 20:47:36 -0400

    Frontend with Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import config from './config';
 import {InventoryRouter} from './routes/inventory';
 import {ShipmentRouter} from './routes/shipment';
+import swaggerJson from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use('/api/inventory', InventoryRouter);
 app.use('/api/shipment', ShipmentRouter);
 
